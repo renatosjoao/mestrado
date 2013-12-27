11f9bec863227909bb99f102bf4e67dfc9f32393
Carlos Santos <carlos.s.santos@gmail.com>
2013-12-27 15:34:26 -0200

    Profiling function for classifier.update_table

--- test/profiling.py
+++ test/profiling.py
@@ -11,9 +11,14 @@ import classifier as cl
 import numpy as np
 import timeit
 
+def run_update_table():
+    print timeit.timeit("cl.update_table(w0, w1, 0.8, dec)", 
+                        setup="import classifier as cl;from profiling import update_table_setup; w0, w1, dec = update_table_setup()", 
+                        number=100)
 
 def run_error():
-    print timeit.timeit("cl.error(w0, w1)", setup="import classifier as cl; from profiling import error_setup; w0, w1 = error_setup()", number=100)
+    print timeit.timeit("cl.error(w0, w1)", 
+                        setup="import classifier as cl;from profiling import error_setup; w0, w1 = error_setup()", number=100)
   
 def error_setup():    
     w0 = np.random.uniform(size=10000)
@@ -22,4 +27,11 @@ def error_setup():
     w0 /= _sum
     w1 /= _sum
     return w0, w1
+    
+def update_table_setup():
+    w0 = np.random.uniform(size=10000)
+    w1 = np.random.uniform(size=10000)
+    dec = cl.make_decision(w0, w1)
+    return w0, w1, dec
+    
     
