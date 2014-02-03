df0ed3b88c6a17ac1d44072ddbd29708a4029ed9
Renato Stoffalette Joao <renatosjoao@gmail.com>
2014-02-03 15:10:22 -0200

    Enabled plotting graphic, changed error file name and saved minterm file
    Signed-off-by: Renato Stoffalette Joao <renatosjoao@gmail.com>

--- scripts/classifier_example.py
+++ scripts/classifier_example.py
@@ -18,13 +18,16 @@ import classifier as cl
 import argparse
 import numpy as np
 import pickle
+import graphics as gra
 
 def main(xpl_data, num_features=None, num_iterations=None, save_todir=None):
     data = xpl_data.data
+    
     # copies frequency data as original frequencies are used towards the end to estimate training error
     w0 = xpl_data.freq0.copy()
     w1 = xpl_data.freq1.copy()
     error_list = []
+    mae_list = []
     DEC = np.zeros(w0.shape)
     GVector = []
     i=0
@@ -32,49 +35,44 @@ def main(xpl_data, num_features=None, num_iterations=None, save_todir=None):
     win = ".win"
     png = ".png"
 
-    file = open(save_todir+"iteration_error.txt", "w")
+    file = open(save_todir+"Error.txt", "w")
 
     for i in range(num_iterations):
-
         indices, feature_list, _ = ft.cmim(data, w0, w1, num_features)
-
         tw.to_window_file(indices, xpl_data.winshape, save_todir+winfile+str(i)+win)
-
         tw.to_image_file(indices,xpl_data.winshape, save_todir+winfile+str(i)+png, scale=8)
-
         w0, w1 = cl.normalize_table(w0, w1)
-
         w0, w1, updated_decision, cls_error =  cl.apply_feature_selection(data, indices, w0, w1)
-
         str_to_file = "Classification error for iteration " + str(i) +" = "+ str(cls_error) +".\n"
-
         file.write(str_to_file)
-
         error_list.append(cls_error)
-
         bt = cl.beta_factor(cls_error)
-
         gam = np.log(1/bt)
         GVector = np.append(GVector,gam)
-
         #DEC represents the Decision Table. Each column represents the decision
         #for an iteration
         DEC = np.column_stack((DEC,updated_decision))
+        aux_dec = DEC
+        aux_dec = np.delete(aux_dec,0, axis=1)
+        hypothesis = cl.adaboost_decision(aux_dec, GVector)
+        MAE_t = cl.mae_from_distribution(hypothesis,w0, w1)
+        mae_list = np.append(mae_list,MAE_t)
+        str_to_file = "MAE for iteration " + str(i) +" = "+ str(MAE_t) +".\n\n"
+        file.write(str_to_file)
+        cl.write_minterm_File(save_todir+"mtm_"+str(i),indices, xpl_data.winshape, data[:, indices], hypothesis)
 
     #Must delete the first column because it contains only Zeros as it was initialized with np.zeros()
     DEC = np.delete(DEC,0, axis=1)
-
+    
     hypothesis = cl.adaboost_decision(DEC, GVector)
-
-    w0_train, w1_train = cl.normalize_table(xpl_data.freq0, xpl_data.freq1)
-
+    w0_train, w1_train = cl.normalize_table(xpl_data.freq0, xpl_data.freq1)    
     MAE = cl.mae_from_distribution(hypothesis,w0_train, w1_train)
     str_to_file = "Final MAE = "+str(MAE)
     file.write(str_to_file)
-    print MAE
-
+    #print MAE
     file.close()
-
+    gra.plot_MAE_iter(np.array(range(num_iterations)), np.array(mae_list))
+        
 if __name__ == "__main__":
      parser = argparse.ArgumentParser(description="Perform t iterations of the ensemble algorithm on a given XPL file.")
 
@@ -96,3 +94,12 @@ if __name__ == "__main__":
      else:
          print "Must provide input file name."
         
+def save_decision(decision_table, file_path):
+    """ Meant to save decision table to file
+    """
+    np.savetxt(file_path, decision_table, fmt="%d")
+
+def save_pattern(pattern_table, file_path):
+    """ Meant to save pattern table to file
+    """
+    np.savetxt(file_path, pattern_table, fmt="%d")
