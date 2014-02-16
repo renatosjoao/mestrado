7fa78d04f200b0c9156ffe9913e46da6f2287a22
Renato Stoffalette Joao <renatosjoao@gmail.com>
2014-02-16 16:20:52 -0300

    Adding a method to build a xpl file.
    ***This file must be compiled with trioslib original source code.
    
    Signed-off-by: Renato Stoffalette Joao <renatosjoao@gmail.com>

---
+++ trios_build_xpl.c
@@ -0,0 +1,42 @@
+/*
+ ============================================================================
+ Name        : trios_build_xpl.c
+ Author      : Renato Stoffalette Joao
+ Version     :
+ Copyright   : Your copyright notice
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "trios.h"
+#include <string.h>
+#include <strings.h>
+
+int main(int argc, char* argv[]) {
+    char *training_set_descr = NULL;
+    char *window_descr = NULL;
+    char *output_file = NULL;
+
+
+    if (argc < 4) {
+
+            printf("\n Usage: build_xpl image_set_path window_path outputfile \n \n ");
+             return -1;
+      }else{
+    	  training_set_descr = argv[1];
+    	  window_descr = argv[2];
+    	  output_file = argv[3];
+
+    	  lcollec(training_set_descr,window_descr,NULL,1,1,0,output_file,NULL);
+
+      }
+	return EXIT_SUCCESS;
+}
+
+void printHelp(){
+printf("********************\n");
+printf("Usage:\n");
+
+}
